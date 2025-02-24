fb571602a125d55464e5ddbb9dd01de52043361d
Alidou KI <alidou@MacBook-Pro-de-Alidou.local>
2025-02-24 17:01:49 +0000

    Commit initial

---
+++ src/main/java/com/tp/Exemple.java
@@ -0,0 +1,16 @@
+package com.tp;
+
+public class Exemple {
+    public static void main(String[] args) {
+
+        String test = null;
+        if(test.equals("Test")) {
+            System.out.println("La chaîne est Test");
+        }
+
+        int x = 10;
+        int y = 20;
+        int somme = x + y;
+    }
+}
+
