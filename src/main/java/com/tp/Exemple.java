baa6b10a7579528c129b061836bad06a7a0ce596
Alidou KI <alidou@MacBook-Pro-de-Alidou.local>
2025-03-01 20:03:47 +0000

    Correction

--- src/main/java/com/tp/Exemple.java
+++ src/main/java/com/tp/Exemple.java
@@ -1,16 +1,25 @@
 package com.tp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Exemple {
+
+    private static final Logger logger = LoggerFactory.getLogger(Exemple.class);
+
     public static void main(String[] args) {
 
-        String test = null;
-        if(test.equals("Test")) {
-            System.out.println("La chaîne est Test");
+        // Éviter NullPointerException
+        String test = "test";
+        if ("Test".equals(test)) {
+            logger.info("La chaîne est Test");
         }
 
         int x = 10;
         int y = 20;
         int somme = x + y;
+
+        // On utilise la variable pour éviter l’erreur "unused"
+        logger.info("La somme est : {}", somme);
     }
 }
-
